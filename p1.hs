12d3cd4fe68de84aa48d6eded82ef8de371f23f0
Jaroslaw Jantura <jjantura@gmail.com>
2015-03-02 18:08:11 +0100

    minor changes in p1

--- p1.hs
+++ p1.hs
@@ -1,3 +1,4 @@
+sum_of_multiplies :: Integer -> Integer -> Integer
 sum_of_multiplies ll ul = sum[x | x<-[ll..ul], x `mod` 3 == 0  || x `mod` 5 == 0]  
 foo = print $ sum_of_multiplies 1 999
 main = foo
