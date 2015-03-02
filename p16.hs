2bcc256dd52de8cc315b40d51c2e7ccdf3d04a5b
Jaroslaw Jantura <jjantura@gmail.com>
2015-03-02 17:22:27 +0100

    problem 16 solved

---
+++ p16.hs
@@ -0,0 +1,6 @@
+number_to_digits :: Integral x => x -> [x]
+number_to_digits 0 = []
+number_to_digits x = number_to_digits (x `div` 10) ++ [x `mod` 10]
+
+main =	print $ sum $ number_to_digits (2^1000)
+
