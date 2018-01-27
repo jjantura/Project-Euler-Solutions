616aeb6b158d6ca36524d192f923972e08ffaf9c
Jarosław Jantura <jjantura@gmail.com>
2018-01-27 15:51:38 +0100

    Add solution for problem 3

--- src/p3.hs
+++
@@ -1,8 +0,0 @@
-prime_factors :: Integer -> [Integer]
-prime_factors n =
-  case factors of
-    [] -> [n]
-    _  -> factors ++ prime_factors (n `div` (head factors))
-  where factors = take 1 $ filter (\x -> (n `mod` x) == 0) [2 .. n - 1]
-main = do
-	print(last $ prime_factors 600851475143)
