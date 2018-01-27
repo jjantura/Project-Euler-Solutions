9614c87e5cdd3497d168f58e86f1459d7ce57ab2
Jarosław Jantura <jjantura@gmail.com>
2018-01-27 16:20:00 +0100

    Fix solution for problem 9

--- src/P009.hs
+++ src/P009.hs
@@ -3,8 +3,8 @@ module P009 (
     p009_solution
 ) where
 
-pythagorean_triplet :: Integer -> [Integer]
-pythagorean_triplet n = [a * b * c | a <- [1..n], b <- [a+1..n - a], c <- [b+1..n - a - b], a + b + c == n, a * a + b * b == c * c]
+pythagorean_triplet :: Integer -> Integer
+pythagorean_triplet n = [a * b * c | a <- [1..n], b <- [a+1..n - a], c <- [b+1..n - a - b], a + b + c == n, a * a + b * b == c * c] !! 0
     
 p009_solution :: String
 p009_solution = show $ pythagorean_triplet 1000
