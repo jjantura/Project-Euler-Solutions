c19ad27b3889a3f8e6f0ecb8db62350b3418dbbc
Jarosław Jantura <jjantura@gmail.com>
2018-01-27 16:17:49 +0100

    Add solution for problem 9

--- src/P009.hs
+++ src/P009.hs
@@ -7,6 +7,6 @@ pythagorean_triplet :: Integer -> [Integer]
 pythagorean_triplet n = [a * b * c | a <- [1..n], b <- [a+1..n - a], c <- [b+1..n - a - b], a + b + c == n, a * a + b * b == c * c]
     
 p009_solution :: String
-p009_solution = pythagorean_triplet 1000
+p009_solution = show $ pythagorean_triplet 1000
 
 
