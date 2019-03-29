c1378c08718f15cac0bd6cf42aaa620a35993fa2
Jaroslaw Jantura <jjantura@gmail.com>
2019-03-29 20:42:25 +0100

    Comment out problem 211

--- src/P211.hs
+++ src/P211.hs
@@ -1,13 +1,14 @@
--- cabal install arithmoi ;)
--- suboptimal
+
 module P211 (
  	
 ) where 
-import Math.NumberTheory.Primes.Factorisation
-import Math.NumberTheory.Powers.Squares
+--import Math.NumberTheory.Primes.Factorisation
+--import Math.NumberTheory.Powers.Squares
 
-p211 :: Integer -> Integer
-p211 limit = sum [i | i <- [1..limit - 1], isSquare' (sigma 2 i)] 
+--p211 :: Integer -> Integer
+--p211 limit = sum [i | i <- [1..limit - 1], isSquare' (sigma 2 i)] 
 
-p211_solution :: String
-p211_solution =	print $  p211 64000000
+--p211_solution :: String
+--p211_solution =	print $  p211 64000000
+
+
